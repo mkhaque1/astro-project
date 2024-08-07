908adb8b2db06c7323d3c4ad5020ff0cc32b6d67
mkhaque1 <mkhaque29@gmail.com>
2024-08-07 22:25:11 +0200

    added 2 components

---
+++ src/components/Button.svelte
@@ -0,0 +1,19 @@
+
+<script lang="ts">
+    import "/public/Vector (4).svg";
+    import FaLongArrowAltRight from 'svelte-icons/fa/FaLongArrowAltRight.svelte';
+    export let title;
+</script>
+
+<button class="button" >{title}
+    <div class="icon"><FaLongArrowAltRight/></div>
+</button>
+
+<style>
+    .icon{
+    width: 18px;
+    display: flex;
+    rotate: -40deg;
+    }
+
+</style>

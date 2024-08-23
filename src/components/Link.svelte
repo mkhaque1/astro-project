cf99a097a97930fe675360395ac1adad02623c09
mkhaque1 <mkhaque29@gmail.com>
2024-08-23 23:30:52 +0200

    service component added

---
+++ src/components/Link.svelte
@@ -0,0 +1,36 @@
+<script>
+     export let href = "";
+     export let text = "hhh";
+     import FaLongArrowAltRight from 'svelte-icons/fa/FaLongArrowAltRight.svelte'
+</script>
+
+<div class="link">
+    <a  {href}>{text}</a>
+    <span>
+        <FaLongArrowAltRight />
+    </span>
+    
+</div>
+
+<style lang="scss">
+    .link {
+        & a{
+        color: #2AA6CD;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        }:hover{
+            color: aliceblue;
+        }
+        display: flex;
+        gap: 10px;
+        
+        & span{
+            color: #2AA6CD;
+            width: 25px;
+            
+        }
+        
+    }
+</style>

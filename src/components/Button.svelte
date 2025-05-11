5423dd404e530d6842e2e89bad480d634b68bac5
mkhaque1 <mkhaque29@gmail.com>
2025-05-11 14:12:33 +0200

    added section in about

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,18 +1,21 @@
-
 <script lang="ts">
     import "/public/Vector (4).svg";
     import FaLongArrowAltRight from 'svelte-icons/fa/FaLongArrowAltRight.svelte';
     export let title;
-    export let href= "";
+    export let href = "";
+    export let className = ""; // Allow passing custom class
 </script>
 
-<a href={href} class="button" >{title}<span class="icon"><FaLongArrowAltRight/></span>
+<a href={href} class="button {className}">
+  {title}
+  <span class="icon"><FaLongArrowAltRight /></span>
 </a>
 
 <style>
-    .icon{
+  .icon {
     width: 18px;
     display: flex;
     rotate: -40deg;
-    }
+    margin-left: 8px;
+  }
 </style>

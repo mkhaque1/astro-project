456037411f201f2c06c77555e157e0927d61b8d8
mkhaque1 <mkhaque29@gmail.com>
2025-10-23 21:47:27 +0200

    mobile menu fix

--- src/components/MobileHeader.svelte
+++ src/components/MobileHeader.svelte
@@ -19,7 +19,6 @@
         <a href="/">Home</a>
         <a href="/about">About</a>
         <a href="/service">Services</a>
-        <a href="/">Projects</a>
         <a href="/contact">Contact</a>
       </nav>
     </div>
